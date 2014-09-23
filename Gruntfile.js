3db93046dd2ab31849eb001e57676a79e932955b
Timotius Nugroho Chandra <timotius.n.chandra@gdpventure.com>
2014-09-23 22:07:04 +0700

    - Added 'Our Story' - Fixed broken glyphicon font

--- Gruntfile.js
+++ Gruntfile.js
@@ -345,7 +345,8 @@ module.exports = function (grunt) {
             'views/{,*/}*.html',
             'scripts/directives/{,*/}{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'bower_components/sass-bootstrap/fonts/*.*'
           ]
         }, {
           expand: true,
